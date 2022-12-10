e8fc82b3248eae083fc201f9e64d380a13dec4c2
C:\Users\Carlo Dee <109793453+cd-Crypton@users.noreply.github.com>
2022-12-11 07:37:40 +0800

    Clean up.

--- twrp_RMX3081.mk
+++ twrp_RMX3081.mk
@@ -24,10 +24,7 @@ $(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
-#$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-#$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-#$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
-#$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := RMX3081
