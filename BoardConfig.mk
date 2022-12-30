a949ac3c82a3cd0e96232b65abb62adb4196985d
Shibu Shaji <97983211+perilouspike@users.noreply.github.com>
2022-12-30 16:40:34 +0530

    Update BoardConfig.mk
    Moved custom recovery-specific configurations to a new directory.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -134,8 +134,9 @@ TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery.fstab
 TARGET_SYSTEM_PROP := $(DEVICE_PATH)/system.prop
 TARGET_VENDOR_PROP := $(DEVICE_PATH)/vendor.prop
 TW_OVERRIDE_SYSTEM_PROPS := \
-    "ro.build.product;ro.build.fingerprint=ro.system.build.fingerprint;ro.build.version.incremental;ro.product.device=ro.product.system.device;ro.product.model=ro.product.system.model;ro.product.name=ro.product.system.name"
 
+               "ro.build.date.utc;ro.bootimage.build.date.utc=ro.build.date.utc;ro.odm.build.date.utc=ro.build.date.utc;ro.product.build.date.utc=ro.build.date.utc;ro.system.build.date.utc=ro.build.date.utc;ro.system_ext.build.date.utc=ro.build.date.utc;ro.vendor.build.date.utc=ro.build.date.utc;ro.build.product;ro.build.fingerprint=ro.system.build.fingerprint;ro.build.version.incremental;ro.product.device=ro.product.system.device;ro.product.model=ro.product.system.model;ro.product.name=ro.product.system.name"
+           
 # Recovery Modules
 TARGET_RECOVERY_DEVICE_MODULES += \
     android.hidl.allocator@1.0 \
@@ -170,8 +171,8 @@ VENDOR_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
 BOOT_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
 TW_INCLUDE_CRYPTO := true
 TW_INCLUDE_CRYPTO_FBE := true
-BOARD_USES_QCOM_FBE_DECRYPTION := true
 TW_INCLUDE_FBE_METADATA_DECRYPT := true
+BOARD_USES_QCOM_FBE_DECRYPTION := true
 TW_USE_FSCRYPT_POLICY := 2
 PLATFORM_VERSION_LAST_STABLE := $(PLATFORM_VERSION)
 
@@ -182,110 +183,9 @@ BOARD_AVB_RECOVERY_KEY_PATH := external/avb/test/data/testkey_rsa4096.pem
 BOARD_AVB_RECOVERY_ROLLBACK_INDEX := 1
 BOARD_AVB_RECOVERY_ROLLBACK_INDEX_LOCATION := 1
 
-# Set brightness path and level
-TW_BRIGHTNESS_PATH := "/sys/class/backlight/panel0-backlight/brightness"
-TW_MAX_BRIGHTNESS := 1000
-TW_DEFAULT_BRIGHTNESS := 500
-
-# TWRP Configuration
-RECOVERY_SDCARD_ON_DATA := true
-TW_THEME := portrait_hdpi
-TW_SCREEN_BLANK_ON_BOOT := true
-TW_NO_SCREEN_BLANK := true
-TW_INPUT_BLACKLIST := "hbtp_vm"
-TW_INCLUDE_NTFS_3G := true
-TARGET_RECOVERY_QCOM_RTC_FIX := true
-TW_EXCLUDE_DEFAULT_USB_INIT := true
-TW_DEVICE_VERSION := perilouspike
-
-# Fix stock .ozip installation
-TW_SKIP_COMPATIBILITY_CHECK := true
-TW_OZIP_DECRYPT_KEY := 0000
-
-# Excludes
-TW_USE_SERIALNO_PROPERTY_FOR_DEVICE_ID := true
-BOARD_SUPPRESS_SECURE_ERASE := true
-
-# Include some binaries
-TW_INCLUDE_LIBRESETPROP := true
-TW_INCLUDE_REPACKTOOLS := true
-TW_INCLUDE_RESETPROP := true
-TW_INCLUDE_BASH := true
-
-# Languages
-TW_EXTRA_LANGUAGES := true
-
-# Qcom EDL mode
-TW_HAS_EDL_MODE := true
-
-# Use Toolbox instead of Busybox
-TW_USE_TOOLBOX := true 
-
-# Debug-tools
-TWRP_INCLUDE_LOGCAT := true
-TARGET_USES_LOGD := true
-
-# Other flags
-TW_NO_LEGACY_PROPS := true
-TW_OVERRIDE_SYSTEM_PROPS := "ro.build.version.sdk" #needed for Android 11 Gapps
-
-# drift/offset
-TW_QCOM_ATS_OFFSET := 1617714502203
-
 # cure for "ELF binaries" problems
 BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
 
 # deal with "error: overriding commands for target" problems
 BUILD_BROKEN_DUP_RULES := true
 BUILD_BROKEN_MISSING_REQUIRED_MODULES := true
-
-# Statusbar icons flags
-TW_STATUS_ICONS_ALIGN := center
-TW_CUSTOM_CPU_POS := 50
-TW_CUSTOM_CLOCK_POS := 300
-TW_CUSTOM_BATTERY_POS := 800
-
-####################################################################
-# SHRP stuffs - specific lines                                     #                        
-####################################################################
-# NOTE - Dont use '-' or blank spaces in flag values , otherwise it will create build errors or other bugs in recovery (Excluding SHRP_PATH,SHRP_REC).
-
-# Mandatory flags
-
-# Path of your SHRP Tree *
-SHRP_PATH := device/realme/RMX3081
-# Maintainer name *
-SHRP_MAINTAINER := perilouspike
-# Device codename *
-SHRP_DEVICE_CODE := RMX3081
-# Recovery Type (It can be treble,normal,SAR) [Only for About Section] *
-SHRP_REC_TYPE := Treble
-# Recovery Type (It can be A/B or A_only) [Only for About Section] *
-SHRP_DEVICE_TYPE := A_only
-# Your device's recovery path, dont use blindly *
-SHRP_REC := /dev/block/bootdevice/by-name/recovery
-
-#Important flags
-
-# Emergency DownLoad mode (0 = no EDL mode, 1 = EDL mode available)
-SHRP_EDL_MODE := 1
-# Storage paths, put your device's storage paths from fstab *
-SHRP_INTERNAL := /sdcard
-SHRP_EXTERNAL := /external_sd
-SHRP_OTG := /usb_otg
-# Put 0 to disable flashlight *
-SHRP_FLASH := 1
-
-# Optional flags
-
-# SHRP padding flag (for rounded corner devices only). You have to change these values according to your device's roundness.
-SHRP_STATUSBAR_RIGHT_PADDING := 20
-SHRP_STATUSBAR_LEFT_PADDING := 80
-# SHRP Express, enables on-the-fly theme patching (also persistent) + persistent lock [Optional]
-SHRP_EXPRESS := true
-# SHRP Dark mode, use this flag to have dark theme set by default [Optional]
-SHRP_DARK := true
-# Max Brightness of LED [Optional]
-SHRP_FLASH_MAX_BRIGHTNESS := 200
-
-#
