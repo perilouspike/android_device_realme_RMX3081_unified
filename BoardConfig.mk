9144a09e707a4006075a2225ec2d7bd3b6cad75e
Shibu Shaji <97983211+perilouspike@users.noreply.github.com>
2022-12-30 09:30:12 +0530

    Update BoardConfig.mk
    Clean Up

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -246,69 +246,7 @@ TW_CUSTOM_CLOCK_POS := 300
 TW_CUSTOM_BATTERY_POS := 800
 
 ####################################################################
-# SHRP-specific lines                                              #                        
-####################################################################
-# NOTE - Dont use '-' or blank spaces in flag values , otherwise it will create build errors or other bugs in recovery (Excluding SHRP_PATH,SHRP_REC).
-
-# Mandatory flags
-
-# Path of your SHRP Tree *
-SHRP_PATH := device/realme/RMX3081
-# Maintainer name *
-SHRP_MAINTAINER := perilouspike
-# Device codename *
-SHRP_DEVICE_CODE := RMX3081
-# Recovery Type (It can be treble,normal,SAR) [Only for About Section] *
-SHRP_REC_TYPE := Treble
-# Recovery Type (It can be A/B or A_only) [Only for About Section] *
-SHRP_DEVICE_TYPE := A_only
-# Your device's recovery path, dont use blindly *
-SHRP_REC := /dev/block/bootdevice/by-name/recovery
-
-#Important flags
-
-# Emergency DownLoad mode (0 = no EDL mode, 1 = EDL mode available)
-SHRP_EDL_MODE := 1
-# Storage paths, put your device's storage paths from fstab *
-SHRP_INTERNAL := /sdcard
-SHRP_EXTERNAL := /external_sd
-SHRP_OTG := /usb_otg
-# Put 0 to disable flashlight *
-SHRP_FLASH := 1
-
-# Optional flags
-
-# SHRP padding flag (for rounded corner devices only). You have to change these values according to your device's roundness.
-SHRP_STATUSBAR_RIGHT_PADDING := 20
-SHRP_STATUSBAR_LEFT_PADDING := 80
-# SHRP Express, enables on-the-fly theme patching (also persistent) + persistent lock [Optional]
-SHRP_EXPRESS := true
-# SHRP Dark mode, use this flag to have dark theme set by default [Optional]
-SHRP_DARK := true
-# Max Brightness of LED [Optional]
-SHRP_FLASH_MAX_BRIGHTNESS := 200
-# Other flags
-TW_NO_LEGACY_PROPS := true
-TW_OVERRIDE_SYSTEM_PROPS := "ro.build.version.sdk" #needed for Android 11 Gapps
-
-# drift/offset
-TW_QCOM_ATS_OFFSET := 1617714502203
-
-# cure for "ELF binaries" problems
-BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
-
-# deal with "error: overriding commands for target" problems
-BUILD_BROKEN_DUP_RULES := true
-BUILD_BROKEN_MISSING_REQUIRED_MODULES := true
-
-# Statusbar icons flags
-TW_STATUS_ICONS_ALIGN := center
-TW_CUSTOM_CPU_POS := 50
-TW_CUSTOM_CLOCK_POS := 300
-TW_CUSTOM_BATTERY_POS := 800
-
-####################################################################
-# SHRP stuffs-specific lines                                       #                        
+# SHRP stuffs - specific lines                                     #                        
 ####################################################################
 # NOTE - Dont use '-' or blank spaces in flag values , otherwise it will create build errors or other bugs in recovery (Excluding SHRP_PATH,SHRP_REC).
 
