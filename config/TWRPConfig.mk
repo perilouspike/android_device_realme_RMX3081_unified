623b4ddda276998eeb10397a7b8dd0cebad65060
Shibu Shaji <97983211+perilouspike@users.noreply.github.com>
2023-01-02 20:49:33 +0530

    Added some properties and flags from BoardConfig.
    Added some props from BC beacuse of congested BoardConfig.

--- config/TWRPConfig.mk
+++ config/TWRPConfig.mk
@@ -1,13 +1,18 @@
-# TWRP Configuration
-
+#################################
+# TWRP - specific Configuration #
+#################################
 RECOVERY_SDCARD_ON_DATA := true
 TW_THEME := portrait_hdpi
 TW_SCREEN_BLANK_ON_BOOT := true
 TW_NO_SCREEN_BLANK := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_INCLUDE_NTFS_3G := true
-TARGET_RECOVERY_QCOM_RTC_FIX := true
 TW_EXCLUDE_DEFAULT_USB_INIT := true
+TW_INCLUDE_FUSE_EXFAT := true
+TW_INCLUDE_FASTBOOTD := true
+TW_USE_SERIALNO_PROPERTY_FOR_DEVICE_ID := true
+
+# Maintainer/Version
 TW_DEVICE_VERSION := perilouspike
 
 # Set brightness path and level
@@ -18,10 +23,6 @@ TW_DEFAULT_BRIGHTNESS := 500
 # Fix stock .ozip installation
 TW_SKIP_COMPATIBILITY_CHECK := true
 TW_OZIP_DECRYPT_KEY := 0000
-
-# Excludes
-TW_USE_SERIALNO_PROPERTY_FOR_DEVICE_ID := true
-BOARD_SUPPRESS_SECURE_ERASE := true
 
 # Include some binaries
 TW_INCLUDE_LIBRESETPROP := true
@@ -42,9 +43,11 @@ TW_USE_TOOLBOX := true
 TWRP_INCLUDE_LOGCAT := true
 TARGET_USES_LOGD := true
 
-# Other flags
-TW_NO_LEGACY_PROPS := true
-TW_OVERRIDE_SYSTEM_PROPS := "ro.build.version.sdk" #needed for Android 11 Gapps
+# Crypto
+TW_INCLUDE_CRYPTO := true
+TW_INCLUDE_CRYPTO_FBE := true
+TW_INCLUDE_FBE_METADATA_DECRYPT := true
+TW_USE_FSCRYPT_POLICY := 2
 
 # drift/offset
 TW_QCOM_ATS_OFFSET := 1617714502203
@@ -54,3 +57,30 @@ TW_QCOM_ATS_OFFSET := 1617714502203
 TW_CUSTOM_CPU_POS := 400
 TW_CUSTOM_CLOCK_POS := 200
 TW_CUSTOM_BATTERY_POS := 800
+
+# Recovery Modules
+TARGET_RECOVERY_DEVICE_MODULES += \
+    android.hidl.allocator@1.0 \
+    android.hidl.memory@1.0 \
+    android.hidl.memory.token@1.0 \
+    libdmabufheap \
+    libhidlmemory \
+    #libion \
+    libnetutils \
+    #libc.so \
+    #libxml2
+
+RECOVERY_LIBRARY_SOURCE_FILES += \
+    $(TARGET_OUT_SHARED_LIBRARIES)/android.hidl.allocator@1.0.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/android.hidl.memory@1.0.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/android.hidl.memory.token@1.0.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libdmabufheap.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libhidlmemory.so \
+    #$(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libnetutils.so \
+    #$(TARGET_OUT_SHARED_LIBRARIES)/libc.so \
+    #$(TARGET_OUT_SHARED_LIBRARIES)/libxml2.so
+    
+    # Property Override
+    TW_OVERRIDE_SYSTEM_PROPS := "ro.build.date.utc;ro.bootimage.build.date.utc=ro.build.date.utc;ro.odm.build.date.utc=ro.build.date.utc;ro.product.build.date.utc=ro.build.date.utc;ro.system.build.date.utc=ro.build.date.utc;ro.system_ext.build.date.utc=ro.build.date.utc;ro.vendor.build.date.utc=ro.build.date.utc;ro.build.product;ro.build.fingerprint=ro.system.build.fingerprint;ro.build.version.incremental;ro.product.device=ro.product.system.device;ro.product.model=ro.product.system.model;ro.product.name=ro.product.system.name"
+    TW_OVERRIDE_SYSTEM_PROPS := "ro.build.version.sdk" #needed for Android 11 Gapps
