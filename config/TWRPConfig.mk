573fc7a8d1b38821f3ca8d619b64e7f8e0575312
Shibu Shaji <97983211+perilouspike@users.noreply.github.com>
2023-01-03 18:58:13 +0530

    Added some Recovery Modules

--- config/TWRPConfig.mk
+++ config/TWRPConfig.mk
@@ -65,10 +65,10 @@ TARGET_RECOVERY_DEVICE_MODULES += \
     android.hidl.memory.token@1.0 \
     libdmabufheap \
     libhidlmemory \
-    #libion \
+    libion \
     libnetutils \
-    #libc.so \
-    #libxml2
+    libc.so \
+    libxml2
 
 RECOVERY_LIBRARY_SOURCE_FILES += \
     $(TARGET_OUT_SHARED_LIBRARIES)/android.hidl.allocator@1.0.so \
@@ -76,12 +76,12 @@ RECOVERY_LIBRARY_SOURCE_FILES += \
     $(TARGET_OUT_SHARED_LIBRARIES)/android.hidl.memory.token@1.0.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/libdmabufheap.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/libhidlmemory.so \
-    #$(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/libnetutils.so \
-    #$(TARGET_OUT_SHARED_LIBRARIES)/libc.so \
-    #$(TARGET_OUT_SHARED_LIBRARIES)/libxml2.so
+    $(TARGET_OUT_SHARED_LIBRARIES)/libc.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libxml2.so
     
     # Property Override
-     TW_OVERRIDE_SYSTEM_PROPS := "ro.build.version.sdk" #needed for Android 11 Gapps
+    TW_OVERRIDE_SYSTEM_PROPS := "ro.build.version.sdk" #needed for Android 11 Gapps
     TW_OVERRIDE_SYSTEM_PROPS := "ro.build.date.utc;ro.bootimage.build.date.utc=ro.build.date.utc;ro.odm.build.date.utc=ro.build.date.utc;ro.product.build.date.utc=ro.build.date.utc;ro.system.build.date.utc=ro.build.date.utc;ro.system_ext.build.date.utc=ro.build.date.utc;ro.vendor.build.date.utc=ro.build.date.utc;ro.build.product;ro.build.fingerprint=ro.system.build.fingerprint;ro.build.version.incremental;ro.product.device=ro.product.system.device;ro.product.model=ro.product.system.model;ro.product.name=ro.product.system.name"
    
