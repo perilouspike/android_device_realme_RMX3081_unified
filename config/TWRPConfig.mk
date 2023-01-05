07fc96b02c221fed3e6ce06d325075597d44133d
Shibu Shaji <97983211+perilouspike@users.noreply.github.com>
2023-01-05 21:43:51 +0530

    Update TWRPConfig.mk
    Nothing more just clean up..

--- config/TWRPConfig.mk
+++ config/TWRPConfig.mk
@@ -83,7 +83,5 @@ RECOVERY_LIBRARY_SOURCE_FILES += \
     
 # Property Override
 #TW_NO_LEGACY_PROPS := true
-TW_OVERRIDE_SYSTEM_PROPS := "" #needed for Android 11 Gapps
-
 TW_OVERRIDE_SYSTEM_PROPS := \
      "ro.build.version.sdk;ro.build.date.utc;ro.bootimage.build.date.utc=ro.build.date.utc;ro.odm.build.date.utc=ro.build.date.utc;ro.product.build.date.utc=ro.build.date.utc;ro.system.build.date.utc=ro.build.date.utc;ro.system_ext.build.date.utc=ro.build.date.utc;ro.vendor.build.date.utc=ro.build.date.utc;ro.build.product;ro.build.fingerprint=ro.system.build.fingerprint;ro.build.version.incremental;ro.product.device=ro.product.system.device;ro.product.model=ro.product.system.model;ro.product.name=ro.product.system.name"
